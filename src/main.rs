2dd742b21d3a42ccf4bb1a20f7dd8bcc07d2839e
nevy11 <smongare2003@gmail.com>
2025-03-20 16:37:03 +0300

    rebuild cargo file

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,3 @@
-use e_learning_cargo::test;
-use test::test_file1::hash_password;
 fn main() {
-    let password = b"Skyworth.95";
-    let hashed_password = hash_password(password);
-    println!("{}", hashed_password);
-    let different_login = b"Skyworth.95";
-    let matches = argon2::verify_encoded(&hashed_password, different_login).unwrap();
-    println!("does it match? {matches}")
+    println!("hello world");
 }
