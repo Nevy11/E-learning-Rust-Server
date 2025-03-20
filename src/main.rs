72f75cbc72a979a11a798deadc17ad29dae8def8
nevy11 <smongare2003@gmail.com>
2025-03-20 18:07:59 +0300

    complete table users creation and connection

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,43 @@
-use std::hash;
-
 use actix_cors::Cors;
 use actix_web::{http, post, web::Json, App, HttpResponse, HttpServer};
 use e_learning_cargo::{
-    hash_password::{hash_password::hash_custom_password, verify_password::verify_password},
-    models::AppUsers,
+    app_users::create_app_user::create_app_user,
+    hash_password::hash_password::hash_custom_password,
+    models::{AppUsers, AppUsersReturn},
 };
 
-#[post("/create_app_user")]
-pub async fn create_app_user(data: Json<AppUsers>) -> HttpResponse {
+#[post("/create_app_user_server")]
+pub async fn create_app_user_server(data: Json<AppUsers>) -> HttpResponse {
     let data_to_create = AppUsers {
-        username: data.username.clone().to_uppercase(),
+        username: data.username.clone(),
         useremail: data.useremail.clone(),
-        userpassword: data.userpassword.clone().to_uppercase(),
+        userpassword: hash_custom_password(&data.userpassword.clone()),
     };
-    HttpResponse::Ok().body("user created")
+    let created_data: Result<AppUsers, diesel::result::Error> = create_app_user(data_to_create);
+    match created_data {
+        Ok(data) => {
+            let return_data = AppUsersReturn {
+                username: data.username.clone(),
+                useremail: data.useremail.clone(),
+                is_successful: true,
+                message: String::new(),
+            };
+            HttpResponse::Ok().json(return_data)
+        }
+        Err(error) => {
+            let return_data = AppUsersReturn {
+                username: data.username.clone(),
+                useremail: data.useremail.clone(),
+                is_successful: false,
+                message: error.to_string(),
+            };
+            HttpResponse::Ok().json(return_data)
+        }
+    }
 }
 
 #[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
-    let hashed_password = hash_custom_password("password");
-    let entered_password = String::from("passworD");
-    let is_user = verify_password(hashed_password, entered_password);
-    if is_user {
-        println!("You are the current user");
-    } else {
-        println!("You are not the current user");
-    }
     HttpServer::new(|| {
         App::new()
             .wrap(
@@ -39,7 +50,7 @@ async fn main() -> Result<(), std::io::Error> {
                     ])
                     .max_age(3600),
             )
-            .service(create_app_user)
+            .service(create_app_user_server)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
--- src/models.rs
+++ src/models.rs
@@ -1,11 +1,19 @@
 use diesel::{pg::Pg, prelude::*};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Insertable, Selectable, Deserialize, Queryable)]
+#[derive(Insertable, Selectable, Deserialize, Queryable, Clone)]
 #[diesel(table_name=crate::schema::app_users)]
 #[diesel(check_for_backend(Pg))]
 pub struct AppUsers {
     pub username: String,
     pub useremail: String,
     pub userpassword: String,
+}
+
+#[derive(Serialize)]
+pub struct AppUsersReturn {
+    pub username: String,
+    pub useremail: String,
+    pub is_successful: bool,
+    pub message: String,
 }
