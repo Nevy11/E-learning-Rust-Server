b2e971a748b32047ec14bc8f2e14734caa3cb7c6
nevy11 <smongare2003@gmail.com>
2025-03-20 17:13:31 +0300

    initialize server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
+use actix_cors::Cors;
+use actix_web::{http, App, HttpServer};
+
 #[actix_web::main]
-async fn main() {
+async fn main() -> Result<(), std::io::Error> {
     println!("hello world");
+    HttpServer::new(|| {
+        App::new().wrap(
+            Cors::default()
+                .allowed_origin("http://localhost:4200")
+                .allowed_methods(vec!["GET", "POST", "DELETE", "PATCH"])
+                .allowed_headers(vec![
+                    http::header::AUTHORIZATION,
+                    http::header::CONTENT_TYPE,
+                ])
+                .max_age(3600),
+        )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
