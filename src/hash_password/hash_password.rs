b803d8044b3d8cc33f471dddd86a140ddedb1b23
nevy11 <smongare2003@gmail.com>
2025-03-26 20:16:33 +0300

    remove print statements

--- src/hash_password/hash_password.rs
+++ src/hash_password/hash_password.rs
@@ -7,10 +7,8 @@ pub fn hash_custom_password(password: &str) -> String {
     let password = password.as_bytes();
     dotenv().ok();
     let salt = env::var("SALT").expect("SALT NOT FOUND\nPLEASE ADD SALT IN THE .env variable");
-    println!("{salt}");
     let salt = salt.as_bytes();
     let config = Config::default();
     let hash = argon2::hash_encoded(password, salt, &config).unwrap();
-    println!("{}", hash);
     hash
 }
